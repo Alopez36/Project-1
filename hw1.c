c76f84802501d5b8d2d272d762c73e4c83e24fb0
Alex <alope6@uic.edu>
2018-01-31 23:15:32 -0600

    added hw1.c file

---
+++ hw1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("%s%d\n", "Alope", 6);
+    return 0;
+}
+
+void all() {
+}
+
+int your;
+
+void cs361() {
+}
+//content
+char are = 'a';
+int belong;
+int to = 0;
+char us[37];
